cd62786fe22ce712fd485c18cb3570b99c726c47
Gautier Paux <gautier@paux.fr>
2017-02-07 09:08:33 +0000

    version 1.0.4

---
+++ NEWS.md
@@ -0,0 +1,50 @@
+# Mediana 1.0.4
+
+## New features
+
+* Addition of the Fixed-sequence procedure (`FixedSeqAdj`, see [Analysis model](http://gpaux.github.io/Mediana/AnalysisModel.html#MultAdjProcobject)).
+
+* Addition of the Cox method to calculate the HR, effect size and ratio of effect size for time-to-event endpoint. This can be accomplished by setting the  `method` argument in the parameter list to set-up the calculation based on the Cox method. (`par = parameters(method = "Cox"`), see [Analysis model](http://gpaux.github.io/Mediana/AnalysisModel.html#Statisticobject)).
+
+* Addition of the package version information in the report.
+* 
+## Bug fixes
+
+* Revision of one-sided p-value computation for Log-Rank test.
+
+* Revision of the call for Statistic in the core function (not visible).
+
+* Revision of the function to calculate the Hazard Ratio Statistic (HazardRatioStat method). By default, this calculation is now based on the log-rank statistic ((O2/E2)/(O1/E1) where O and E are Observed and Expected event in sample 2 and sample 1. A parameter can be added using the `method` argument in the parameter list to set-up the calculation based on the Cox method (`par = parameters(method = "Cox"`), see [Analysis model](http://gpaux.github.io/Mediana/AnalysisModel.html#Statisticobject)).
+
+* Revision of the function to calculate the effect size for time-to-event endpoint (`EffectSizeEventStat` method, based on the `HazardRatioStat` method)
+
+* Revision of the functions to calculate the ratio of effect size for continuous (`RatioEffectSizeContStat` method), binary (`RatioEffectSizePropStat` method) and event (`RatioEffectSizeEventStat method`) endpoint.
+
+* Revision of the function to generate the Test, Statistic, Design and result tables in the report.
+
+# Mediana 1.0.3
+
+## New features
+
+* Addition of the Beta distribution (`BetaDist`, see [Data model](http://gpaux.github.io/Mediana/DataModel.html#OutcomeDistobject)).
+
+* Addition of the Truncated exponential distribution, which could be used as enrollment distribution (`TruncatedExpoDist`, see [Data model](http://gpaux.github.io/Mediana/DataModel.html#OutcomeDistobject)).
+
+* Addition of the Non-inferiority test for proportion (`PropTestNI`, see [Analysis model](http://gpaux.github.io/Mediana/AnalysisModel.html#Testobject)).
+
+* Addition of the mixture-based gatekeeping procedure (`MixtureGatekeepingAdj` see [Analysis model](http://gpaux.github.io/Mediana/AnalysisModel.html#MultAdjProcobject)).
+
+* Addition of a function to get the data generated in the CSE using the `DataStack` function (see [Data stack](http://gpaux.github.io/Mediana/DataStack.html)).
+
+* Addition of a function to extract a specific set of data in a `DataStack` object (see [Data stack](http://gpaux.github.io/Mediana/DataStack.html#ExtractDataStack)).
+
+* Addition of the "Evaluation Model" section in the generated report describing the criteria and their parameters (see [Simulation report](http://gpaux.github.io/Mediana/Reporting.html#Description18)).
+
+
+## Bug fixes
+
+* Revision of the generation of dropout time.
+
+* Correction of the `NormalParamAdj` function.
+
+* Correction of the `FisherTest` function.
--- R/EffectSizeCoxEventStat.R
+++ R/EffectSizeCoxEventStat.R
@@ -1,18 +1,18 @@
 ######################################################################################################################
 
-# Function: LogrankTest .
-# Argument: Data set and parameter.
-# Description: Computes one-sided p-value based on log-rank test.
+# Compute the log hazard ratio based on non-missing values in the combined sample
 
-LogrankTest = function(sample.list, parameter) {
-
-  # Determine the function call, either to generate the p-value or to return description
+EffectSizeCoxEventStat = function(sample.list, parameter) {
+  
+  # Determine the function call, either to generate the statistic or to return description
   call = (parameter[[1]] == "Description")
-
+  
   if (call == FALSE | is.na(call)) {
-
-    # Sample list is assumed to include two data frames that represent two analysis samples
-
+    
+    # Error checks
+    if (length(sample.list)!=2)
+      stop("Analysis model: Two samples must be specified in the EffectSizeCoxEventStat statistic.")
+    
     # Outcomes in Sample 1
     outcome1 = sample.list[[1]][, "outcome"]
     # Remove the missing values due to dropouts/incomplete observations
@@ -22,7 +22,7 @@ LogrankTest = function(sample.list, parameter) {
     event1.complete = event1[stats::complete.cases(outcome1)]
     # Sample size in Sample 1
     n1 = length(outcome1.complete)
-
+    
     # Outcomes in Sample 2
     outcome2 = sample.list[[2]][, "outcome"]
     # Remove the missing values due to dropouts/incomplete observations
@@ -32,40 +32,21 @@ LogrankTest = function(sample.list, parameter) {
     event2.complete = event2[stats::complete.cases(outcome2)]
     # Sample size in Sample 2
     n2 = length(outcome2.complete)
-
+    
     # Create combined samples of outcomes, censoring indicators (all events are observed) and treatment indicators
     outcome = c(outcome1.complete, outcome2.complete)
     event = c(event1.complete, event2.complete)
     treatment = c(rep(0, n1), rep(1, n2))
-
-    data = data.frame(time = outcome,
-                      event = event,
-                      treatment = treatment)
-    data = data[order(data$time),]
-    data$event1 = data$event*(data$treatment==0)
-    data$event2 = data$event*(data$treatment==1)
-    data$eventtot = data$event1 + data$event2
-    data$n.risk1.prior = length(outcome1) - cumsum(data$treatment==0) + (data$treatment==0)
-    data$n.risk2.prior = length(outcome2) - cumsum(data$treatment==1) + (data$treatment==1)
-    data$n.risk.prior = data$n.risk1.prior + data$n.risk2.prior
-
-    data$e1 = data$n.risk1.prior*data$eventtot/data$n.risk.prior
-    data$u1 = data$event1 - data$e1
-    data$v1 = ifelse(data$n.risk.prior > 1,
-                     (data$n.risk1.prior*data$n.risk2.prior*data$eventtot*(data$n.risk.prior - data$eventtot))/(data$n.risk.prior**2*(data$n.risk.prior-1)),
-                     0)
-
-    stat.test = sum(data$u1)/sqrt(sum(data$v1))
-
-    # Compute one-sided p-value
-    result = stats::pnorm(stat.test, lower.tail = FALSE)
-
+    
+    # Get the log HR from the Log-Rank method
+    result = log(summary(survival::coxph(survival::Surv(outcome, event) ~ treatment))$coef[,"exp(coef)"])
+    
   }
-
+  
   else if (call == TRUE) {
-    result = list("Log-rank test")
+    result = list("Effect size (event Cox)")
   }
-
+  
   return(result)
 }
-# End of LogrankTest
+# End of EffectSizeCoxEventStat
