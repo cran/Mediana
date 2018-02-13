c63b5811ef7efa5a55f94da86401f76a77e730b6
Gautier Paux <gautier@paux.fr>
2018-02-13 17:01:22 +0000

    version 1.0.6

--- R/OrdinalLogisticRegTest.R
+++ R/OrdinalLogisticRegTest.R
@@ -1,10 +1,10 @@
 ######################################################################################################################
 
-# Function: GLMPoissonTest .
+# Function: OrdinalLogisticRegTest
 # Argument: Data set and parameter (call type).
-# Description: Computes one-sided p-value based on Poisson regression.
-#' @importFrom stats poisson
-GLMPoissonTest = function(sample.list, parameter) {
+# Description: Computes one-sided p-value based on Ordinal Logistic regression.
+
+OrdinalLogisticRegTest = function(sample.list, parameter) {
 
   # Determine the function call, either to generate the p-value or to return description
   call = (parameter[[1]] == "Description")
@@ -16,9 +16,9 @@ GLMPoissonTest = function(sample.list, parameter) {
       larger = TRUE
     }
     else {
-      if (!all(names(parameter[[2]]) %in% c("larger"))) stop("Analysis model: GLMPoissonTest test: this function accepts only one argument (larger)")
+      if (!all(names(parameter[[2]]) %in% c("larger"))) stop("Analysis model: OrdinalRegTest test: this function accepts only one argument (larger)")
       # Parameters are defined but not the larger argument
-      if (!is.logical(parameter[[2]]$larger)) stop("Analysis model: GLMPoissonTest test: the larger argument must be logical (TRUE or FALSE).")
+      if (!is.logical(parameter[[2]]$larger)) stop("Analysis model: OrdinalRegTest test: the larger argument must be logical (TRUE or FALSE).")
       larger = parameter[[2]]$larger
     }
 
@@ -35,20 +35,21 @@ GLMPoissonTest = function(sample.list, parameter) {
     outcome2.complete = outcome2[stats::complete.cases(outcome2)]
 
     # Data frame
-    data.complete = data.frame(rbind(cbind(2, outcome2.complete), cbind(1, outcome1.complete)))
+    data.complete = data.frame(rbind(cbind(2, outcome2.complete),
+                                     cbind(1, outcome1.complete)))
     colnames(data.complete) = c("TRT", "RESPONSE")
     data.complete$TRT=as.factor(data.complete$TRT)
+    # Order the level of the response
+    data.complete$RESPONSE=factor(data.complete$RESPONSE, levels = 1:max(data.complete$RESPONSE), ordered = TRUE)
 
     # One-sided p-value (to be checked)
-    # result = summary(stats::glm(RESPONSE ~ TRT, data = data.complete, family = poisson))$coefficients["TRT2", "Pr(>|z|)"]/2
-    z = summary(stats::glm(RESPONSE ~ TRT, data = data.complete, family = poisson))$coefficients["TRT2", "z value"]
+    z = summary(MASS::polr(RESPONSE ~ TRT, data = data.complete, Hess = TRUE))$coefficients["TRT2", "t value"]
     result = stats::pnorm(z, lower.tail = !larger)
-
   }
   else if (call == TRUE) {
-    result=list("Poisson regression test")
+    result=list("Ordinal logistic regression test")
   }
 
   return(result)
 }
-# End of GLMPoissonTest
+# End of OrdinalRegTest
